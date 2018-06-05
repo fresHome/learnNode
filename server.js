8c11ad9ace59419bb201f43d277e2b09604aa0d5
Ashe <fangjia187@163.com>
2018-06-05 22:22:15 +0800

    2018.6.5 kindle位置#770 64% 让请求处理程序作出相应 阻塞与非阻塞

--- server.js
+++ server.js
@@ -2,14 +2,14 @@
 var http=require("http");
 var url=require("url");
 
-function start(route){
+function start(route,handle){
     function onRequest(req,res){
         var pathname=url.parse(req.url).pathname;
         console.log("Request for"+pathname+"received.");
 
-        route(pathname);
         res.writeHead(200,{"Content-Type":"text/plain"});
-        res.write("Hello World");
+        var content=route(handle,pathname);
+        res.write(content);
         res.end();
     }
     //向http模块提供的函数createServer传递了一个函数
